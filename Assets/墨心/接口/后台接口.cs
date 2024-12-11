bbb8d2f69424dbf88b2c64b8be94d2a34713c320
AX0201 <136287080+AX0201@users.noreply.github.com>
2024-12-11 22:12:09 +0800

    接口拆分：文件太大了

--- Assets/墨心/接口/后台接口.cs
+++ Assets/墨心/接口/后台接口.cs
@@ -23,50 +23,6 @@ namespace 墨心 {
         public float 移动速度 { get; set; }
         public float 旋转速度 { get; set; }
     }
-    public interface I层级 {
-
-    }
-    public interface I地板层 : I层级 {
-        public 地板种类 类型 { get; set; }
-        public int 数量 { get; set; }
-    }
-    public interface I建筑层 : I层级 {
-        public 建筑种类 类型 { get; set; }
-        public int 数量 { get; set; }
-    }
-    public interface I矿石层 : I层级 {
-        public 矿石种类 类型 { get; set; }
-        public int 数量 { get; set; }
-    }
-    public interface I土质层 : I层级 {
-        public 土质种类 类型 { get; set; }
-    }
-    public interface I悬浮层 : I层级 {
-        public 悬浮种类 类型 { get; set; }
-        public int 数量 { get; set; }
-    }
-    public enum 地板种类 {
-        无,
-        石地板
-    }
-    public enum 建筑种类 {
-        无,
-        树木,
-        箱子
-    }
-    public enum 矿石种类 {
-        无,
-        铜矿,
-        铁矿,
-    }
-    public enum 土质种类 {
-        虚空,
-        沙漠,
-        淡水
-    }
-    public enum 悬浮种类 {
-        无
-    }
     public interface I物品 {
         public string 名称 {  get; set; }
         public int 数量 { get; set; }

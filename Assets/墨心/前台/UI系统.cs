82290ccd525078a9a017d2601805045e52316d76
AX0201 <136287080+AX0201@users.noreply.github.com>
2025-01-13 08:04:39 +0800

    背包UI

--- Assets/墨心/前台/UI系统.cs
+++ Assets/墨心/前台/UI系统.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using static 墨心.LocalStorage;
@@ -30,8 +28,9 @@ namespace 墨心.Task8 {
         public void 更新背包显示(I背包 X) {
             背包面板.Clear();
             for (int i = 0; i < X.物品列表.Count; i++) {
-                var A = 背包面板.创建矩形().SetColorDirectly(Color.gray);
-                A.GetComponent<Image>().sprite = Resources.Load<Sprite>("背包" + X.物品列表[i].名称);
+                //var A = 背包面板.创建矩形().SetColorDirectly(Color.gray);
+                //A.GetComponent<Image>().sprite = Resources.Load<Sprite>("背包" + X.物品列表[i].名称);
+                背包面板.创建背包格子(X.物品列表[i]);
             }
         }
         public void 开关背包(bool X) {
@@ -39,11 +38,21 @@ namespace 墨心.Task8 {
         }
     }
     public static partial class LocalStorage {
-        public static void 创建背包格子() {
-
+        public static 背包格子驱动 创建背包格子(this GameObject X, I物品 Y) {
+            return X.创建矩形().AddComponent<背包格子驱动>().SetData(Y);
         }
     }
-    public class 背包格子 {
-
+    public class 背包格子驱动 : MonoBehaviour {
+        public GameObject 垫子层;
+        public GameObject 道具层;
+        public 背包格子驱动 SetData(I物品 X = null) {
+            垫子层?.Destory();
+            道具层?.Destory();
+            垫子层 = gameObject.创建矩形().SetColorDirectly(Color.gray);
+            if (X != null) {
+                道具层 = 垫子层.创建矩形().SetSprite("背包" + X.名称);
+            }
+            return this;
+        }
     }
 }
